899f4a1975be3680e4142d3d866eca9e1a75ed35
Glenn Dirkx <glenn.dirkx@gmail.com>
2022-04-29 10:55:28 +0200

    move test data structs to separate file

--- tests/api_example.rs
+++ tests/api_example.rs
@@ -1,89 +1,21 @@
-#[derive(Debug, PartialEq, Default, ::twpb_derive::Message)]
-struct Message {
-    #[twpb(oneof,nr="1-2")]
-    content: ::core::option::Option<message::Content>,
-}
+mod types;
 
-mod message {
-    #[derive(PartialEq, Debug, ::twpb_derive::Enum)]
-    pub enum Content {
-        #[twpb(message,nr=1)]
-        V1Request(super::v1::Request),
-        #[twpb(message,nr=2)]
-        V1Response(super::v1::Response),
-    }
-}
-
-mod v1 {
-    #[derive(Debug, PartialEq, Default, ::twpb_derive::Message)]
-    pub struct EmptyRequest {}
-
-    #[derive(Debug, PartialEq, Default, ::twpb_derive::Message)]
-    pub struct Request {
-        #[twpb(oneof,nr="1-2")]
-        pub request: ::core::option::Option<request::Request>,
-    }
-
-    #[derive(Debug, PartialEq, Default, ::twpb_derive::Message)]
-    pub struct Response {
-        #[twpb(oneof,nr="1-2")]
-        pub response: ::core::option::Option<response::Response>,
-    }
-
-    #[derive(Debug, PartialEq, Default, ::twpb_derive::Message)]
-    pub struct SysInfo{
-        #[twpb(string,nr=1)]
-        pub serial: heapless::String<10>,
-        #[twpb(string,nr=2)]
-        pub firmware_version: heapless::String<10>,
-        #[twpb(string,nr=3)]
-        pub vendor: heapless::String<10>,
-        #[twpb(string,nr=4)]
-        pub product: heapless::String<10>,
-    }
-
-    #[derive(Debug, PartialEq, Default, ::twpb_derive::Message)]
-    pub struct OtherThing{
-        #[twpb(string,nr=1)]
-        pub other: heapless::String<10>,
-    }
-
-    pub mod request {
-        #[derive(PartialEq, Debug, ::twpb_derive::Enum)]
-        pub enum Request {
-            #[twpb(message,nr=1)]
-            GetInfo(super::EmptyRequest),
-            #[twpb(message,nr=2)]
-            GetOtherThing(super::EmptyRequest),
-        }
-    }
-
-    pub mod response {
-        #[derive(PartialEq, Debug, ::twpb_derive::Enum)]
-        pub enum Response {
-            #[twpb(message,nr=1)]
-            Info(super::SysInfo),
-            #[twpb(message,nr=2)]
-            OtherThing(super::OtherThing),
-        }
-    }
-}
-
+use types::{APIMessage, apimessage, v1};
 
 #[test] // We can successfully decode a getInfo API request
-fn test_get_info(){
+fn test_get_info() {
     let dummydata = include_bytes!("files/bin/python.api.getInfo.bin");
-    let message = Message::twpb_decode_iter(dummydata.iter()).unwrap();
+    let message = APIMessage::twpb_decode_iter(dummydata.iter()).unwrap();
 
     // show-off version
     match message.content {
-        Some(message::Content::V1Request(v1::Request{request: Some(v1::request::Request::GetInfo(_))})) => (),
+        Some(apimessage::Content::V1Request(v1::Request{request: Some(v1::request::Request::GetInfo(_))})) => (),
         _ => panic!("unexpected api message content"),
     }
 
     // what you're likely gonna actually use
     match message.content {
-        Some(message::Content::V1Request(message)) => match message.request {
+        Some(apimessage::Content::V1Request(message)) => match message.request {
             Some(v1::request::Request::GetInfo(_)) => (),
             _ => panic!("wrong request type"),
         },
@@ -93,12 +25,12 @@ fn test_get_info(){
 
 #[test] // A getInfo API request does not decode as something else
 #[should_panic(expected = "wrong request type")]
-fn test_get_info_2(){
+fn test_get_info_2() {
     let dummydata = include_bytes!("files/bin/python.api.getInfo.bin");
-    let message = Message::twpb_decode_iter(dummydata.iter()).unwrap();
+    let message = APIMessage::twpb_decode_iter(dummydata.iter()).unwrap();
 
     match message.content {
-        Some(message::Content::V1Request(message)) => match message.request {
+        Some(apimessage::Content::V1Request(message)) => match message.request {
             Some(v1::request::Request::GetOtherThing(_)) => (),
             _ => panic!("wrong request type"),
         },
--- tests/derive_macro.rs
+++ tests/derive_macro.rs
@@ -1,39 +1,9 @@
-#[derive(Debug, PartialEq, Default, ::twpb_derive::Message)]
-pub struct Simple{
-    #[twpb(string,nr=1)]
-    serial: heapless::String<10>,
-    #[twpb(string,nr=2)]
-    firmware_version: heapless::String<10>,
-    #[twpb(string,nr=3)]
-    vendor: heapless::String<10>,
-    #[twpb(string,nr="4")]
-    product: heapless::String<10>,
-}
+mod types;
 
-#[derive(Debug, PartialEq, Default, ::twpb_derive::Message)]
-pub struct Message {
-    #[twpb(oneof,nr="1-3")]
-    content: ::core::option::Option<message::Content>,
-    // To test that our oneof (which contains a Message object)
-    // consumes only the bytes its supposed to, we have an extra
-    // value after its bytestream.
-    #[twpb(string,nr=5)]
-    something_else: heapless::String<20>,
-}
-
-mod message {
-    #[derive(PartialEq, Debug, ::twpb_derive::Enum)]
-    pub enum Content {
-        #[twpb(message,nr=1)]
-        Ss(super::Simple),
-        #[twpb(string,nr=3)]
-        Test(heapless::String<10>),
-    }
-}
+use types::{Simple, Embedded, embedded};
 
 #[test]
 fn test_simple(){
-    // let dummydata = [0 as u8,0,1];
     let dummydata = include_bytes!("files/bin/python.simple.bin");
 
     let parsed = Simple::twpb_decode_iter(dummydata.iter()).unwrap();
@@ -51,9 +21,9 @@ fn test_simple(){
 fn test_oneof_simple(){
     let dummydata = include_bytes!("files/bin/python.oneof.simple.bin");
 
-    let parsed = Message::twpb_decode_iter(dummydata.iter()).unwrap();
-    let expected = Message {
-        content: Some(message::Content::Test(heapless::String::from("teststr"))),
+    let parsed = Embedded::twpb_decode_iter(dummydata.iter()).unwrap();
+    let expected = Embedded {
+        content: Some(embedded::Content::Test(heapless::String::from("teststr"))),
         something_else: heapless::String::from(""),
     };
     assert_eq!(parsed, expected);
@@ -64,9 +34,9 @@ fn test_oneof_simple(){
 fn test_oneof_embedded(){
     let dummydata = include_bytes!("files/bin/python.oneof.embedded.bin");
 
-    let parsed = Message::twpb_decode_iter(dummydata.iter()).unwrap();
-    let expected = Message {
-        content: Some(message::Content::Ss(Simple{
+    let parsed = Embedded::twpb_decode_iter(dummydata.iter()).unwrap();
+    let expected = Embedded {
+        content: Some(embedded::Content::Ss(Simple{
             serial: heapless::String::from("serial"),
             firmware_version: heapless::String::from("firmware"),
             vendor: heapless::String::from("vendor"),
